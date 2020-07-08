c72888b8fe8644bf03246320fa62c600c66a1a7d
Matthew Hammer <matthew@dfinity.org>
2020-07-08 13:47:24 -0600

    minor nit: test script output

--- test.sh
+++ test.sh
@@ -2,10 +2,15 @@ echo PATH = $PATH
 echo vessel @ `which vessel`
 
 echo
-echo == Build
+echo == Build.
 echo
 
 dfx start --background
 dfx build
+
+echo
+echo == Test.
+echo
+
 dfx canister install --all
 dfx canister call Test test '()'
